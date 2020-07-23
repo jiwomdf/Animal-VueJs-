810a0060cb5b7ed94ce21c04d84dc208e397d0b4
jiwomdf <mdf.jiwo@outlook.com>
2020-07-23 23:08:08 +0700

    create refresher.js

--- src/components/PostPhoto.vue
+++ src/components/PostPhoto.vue
@@ -4,12 +4,16 @@
       <v-col cols="12" sm="10" md="8" lg="6">
         <h1>Post Photo</h1>
         <v-text-field v-model="title" hint="At least 3 characters" label="Username"></v-text-field>
+        <v-btn v-on:click="getPost()">Show</v-btn>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+const axios = require("axios");
+const refresher = require("../refresher");
+
 export default {
   name: "Main",
   data() {
@@ -17,13 +21,55 @@ export default {
       title: "",
     };
   },
-  mounted() {
-    let accessToken = this.$cookie.get("accessToken");
-    let refreshToken = this.$cookie.get("refreshToken");
-    console.log(accessToken, refreshToken);
-  },
   methods: {
-    async login() {},
+    getPost() {
+      const fun = async (accessToken) => {
+        return axios.get("http://localhost:3000/blog", {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+      };
+
+      refresher(fun, this);
+
+      /* let accessToken = this.$cookie.get("accessToken");
+      let refreshToken = this.$cookie.get("refreshToken");
+
+      try {
+        const retval = await axios.get("http://localhost:3000/blog", {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        console.log(retval);
+      } catch (error) {
+        if (error.response.status == 403) {
+          const url = "http://localhost:3000/auth/token/";
+          const data = {
+            userName: this.$store.state.loginUserName,
+            refreshToken: refreshToken,
+          };
+          const headers = {
+            "Content-Type": "application/json",
+          };
+
+          let retVal = await axios.post(url, data, headers);
+
+          console.log(retVal.data.accessToken);
+          this.$cookie.set(retVal.data.accessToken);
+
+          const retval = await axios.get("http://localhost:3000/blog", {
+            headers: {
+              Authorization: `Bearer ${retVal.data.accessToken}`,
+            },
+          });
+
+          console.log(retval);
+        }
+      }*/
+    },
   },
 };
 </script>
---
+++ src/refresher.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+
+module.exports = async (fun, context) => {
+
+    let accessToken = context.$cookie.get("accessToken");
+    let loginUserName = context.$store.state.loginUserName;
+    let refreshToken = context.$cookie.get("refreshToken");
+
+    try {
+        const retval = await fun(accessToken)
+        console.log(retval);
+
+    } catch (error) {
+
+        if (error.response.status == 403) {
+            const url = "http://localhost:3000/auth/token/";
+            const data = { userName: loginUserName, refreshToken: refreshToken, };
+            const headers = { "Content-Type": "application/json", };
+
+            let retVal = await axios.post(url, data, headers);
+
+            console.log("Unauthorized")
+            context.$cookie.delete("accessToken");
+            context.$cookie.set("accessToken", retVal.data.accessToken);
+
+            const retval = await fun(retVal.data.accessToken)
+
+            console.log(retval);
+        }
+    }
+}
